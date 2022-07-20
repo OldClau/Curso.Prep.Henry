0692ab96e551b95d57bbd1eb8a6a8f1a223f1d66
OldClau <claudia@mafe.com.ar>
2022-07-20 11:52:50 -0300

    Ejercicio 02-JS-I

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -196,7 +196,7 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  var z = nombre. + " " + apellido
+  var z = nombre + " " + apellido
   return z
 }
 
